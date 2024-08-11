24bebe1d2690ae89ad2007f14ebb795006e0c0ce
Tulasiram <tulasiramkancharla119@gmail.com>
2024-08-11 13:02:28 +0530

    all complete

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -29,21 +29,16 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
-
 	if err != nil {
 		return err
 	}
-
 	js = append(js, '\n')
 
 	for key, value := range headers {
 		w.Header()[key] = value
 	}
-
 	w.Header().Set("Content-Type", "application/json")
-
 	w.WriteHeader(status)
-
 	w.Write(js)
 	return nil
 }
